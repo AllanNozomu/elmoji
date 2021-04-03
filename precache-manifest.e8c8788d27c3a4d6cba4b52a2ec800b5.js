e294dfd4a2ddac20b661f0b909c1b5c0168c134e
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2021-04-02 21:31:28 -0300

    Updates

---
+++ precache-manifest.e8c8788d27c3a4d6cba4b52a2ec800b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf4f16848a2afec0e654a438b5aa22f",
+    "url": "/elmoji/favicon.ico"
+  },
+  {
+    "revision": "95dd8b9bb70315a66ce2aeac27871953",
+    "url": "/elmoji/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elmoji/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elmoji/manifest.json"
+  },
+  {
+    "revision": "f087d4198176d37a1897",
+    "url": "/elmoji/static/css/main.ca883688.chunk.css"
+  },
+  {
+    "revision": "f087d4198176d37a1897",
+    "url": "/elmoji/static/js/main.f087d419.chunk.js"
+  },
+  {
+    "revision": "1a87473cca6cb67ff568",
+    "url": "/elmoji/static/js/runtime~main.1a87473c.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/elmoji/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
